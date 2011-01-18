588afbfbd3b8728296db7ce41ad51cf5fa8920a4
Douglas Hubler <dhubler@ezuce.com>
2011-01-18 07:29:11 -0500

    use git to just track revision

--- .sipxecs.mk
+++ .sipxecs.mk
@@ -1,12 +1,12 @@
 # format:  ${number of commits past tag}.${refid of HEAD}
-freeswitch_PACKAGE_REVISION = $(shell \
+freeswitch_VER = 1.0.7
+freeswitch_REV = $(shell \
 	cd $(SRC)/$(PROJ); \
-	$(SRC)/config/git-version-gen .version | \
-	sed 's/\([0-9.]\+\)-\([0-9]\+\)-\([0-9a-f]\+\)/\2.\3/g')
+	$(SRC)/config/revision-gen $(freeswitch_VER))
 
-freeswitch_SRPM = freeswitch-1.0.7-$(freeswitch_PACKAGE_REVISION).src.rpm
+freeswitch_SRPM = freeswitch-$(freeswitch_VER)-$(freeswitch_REV).src.rpm
 freeswitch_SPEC = $(SRC)/$(PROJ)/freeswitch.spec
-freeswitch_TARBALL = $(BUILDDIR)/$(PROJ)/freeswitch-1.0.7.tar.bz2
+freeswitch_TARBALL = $(BUILDDIR)/$(PROJ)/freeswitch-$(freeswitch_VER).tar.bz2
 freeswitch_SOURCES = $(freeswitch_TARBALL) \
 	celt-0.7.1.tar.gz \
 	flite-1.3.99-latest.tar.gz \
@@ -20,8 +20,8 @@ freeswitch_SOURCES = $(freeswitch_TARBALL) \
 	communicator_semi_6000_20080321.tar.gz \
 	libmemcached-0.32.tar.gz
 
-freeswitch_SRPM_DEFS = --define "buildno $(freeswitch_PACKAGE_REVISION)"
-freeswitch_RPM_DEFS = --define="buildno $(freeswitch_PACKAGE_REVISION)"
+freeswitch_SRPM_DEFS = --define "buildno $(freeswitch_REV)"
+freeswitch_RPM_DEFS = --define="buildno $(freeswitch_REV)"
 
 # we could, but we don't these targets. FS doesn't support running ./configure from anywhere but source root.
 freeswitch.autoreconf freeswitch.configure:;
@@ -29,4 +29,4 @@ freeswitch.autoreconf freeswitch.configure:;
 freeswitch.dist :
 	test -d $(dir $(freeswitch_TARBALL)) || mkdir -p $(dir $(freeswitch_TARBALL))
 	cd $(SRC)/$(PROJ); \
-	  git archive --format tar --prefix freeswitch-1.0.7/ HEAD | bzip2 > $(freeswitch_TARBALL)
+	  git archive --format tar --prefix freeswitch-$(freeswitch_VER)/ HEAD | bzip2 > $(freeswitch_TARBALL)
