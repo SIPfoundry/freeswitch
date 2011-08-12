551cdf47a5de3316d4912b5c82aead24e9979147
Douglas Hubler <dhubler@ezuce.com>
2011-08-12 10:55:37 -0400

    update to the latest FS, bug fix we are particularly looking for was delay_echo actin in commit f10566a

--- .sipxecs.mk
+++ .sipxecs.mk
@@ -9,11 +9,11 @@ freeswitch_SOURCES = $(freeswitch_TARBALL) \
 	flite-1.3.99-latest.tar.gz \
 	lame-3.97.tar.gz \
 	libshout-2.2.2.tar.gz \
-	mpg123.tar.gz \
+	mpg123-1.13.2.tar.gz \
 	openldap-2.4.11.tar.gz \
-	pocketsphinx-0.5.99-20091212.tar.gz \
-	soundtouch-1.3.1.tar.gz \
-	sphinxbase-0.4.99-20091212.tar.gz \
+	pocketsphinx-0.7.tar.gz \
+	soundtouch-1.5.0.tar.gz \
+	sphinxbase-0.7.tar.gz \
 	communicator_semi_6000_20080321.tar.gz \
 	libmemcached-0.32.tar.gz \
 	json-c-0.9.tar.gz \
